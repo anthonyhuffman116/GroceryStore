3981da8719589646b1e0da486c6db51d1eacb081
marcoqlathrop <marcoqlathrop@gmail.com>
2020-07-28 15:02:57 -0400

    Created shopping-cart.js, price fix in my aisle

---
+++ scripts/shopping-cart.js
@@ -0,0 +1,10 @@
+// Shi Qi Zhou - 40163947
+
+function toggleDescription() {
+    var el = document.getElementById("long-desc");
+    if (el.style.display === "none") {
+        el.style.display = "block";
+    } else {
+        el.style.display = "none";
+    }
+}
