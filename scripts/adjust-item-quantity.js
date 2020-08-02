34321efc79129be0060693da23f627f1b286b5e9
marcoqlathrop <marcoqlathrop@gmail.com>
2020-08-02 13:11:46 -0400

    Er (#47)
    * adjustOrderSummary function
    
    I added the adjustOrderSummary function
    and modified the index
    
    * Update index.html
    
    * minor syntax correction on one line
    
    * Almost finished adjustOrderSummary
    
    I'm not sure why it does not work
    Ill also have to adjust the styles
    
    * er
    
    * er
    
    * Update adjust-item-quantity.js
    
    Co-authored-by: SarahZhou2018 <45517534+SarahZhou2018@users.noreply.github.com>

--- scripts/adjust-item-quantity.js
+++ scripts/adjust-item-quantity.js
@@ -1,13 +1,23 @@
+window.onload = function(){
+    adjustOrderSummary();
+}
+document.getElementById("iconButton").addEventListener("click", adjustOrderSummary());
 
+var items = document.getElementById("itemtable");
+var order = document.getElementById("mytable");
 
-function adjustOrderSummary(name, priceUnit, newNumber) {
-    if(newNumber>0){
-        document.getElementById(name).style.display= "block";
-        document.getElementById(name).getElementById(price).innerHTML=(newNumber * priceUnit).toFixed(2) + " $";
-        document.getElementById(name).getElementById(number).innerHTML="x " + newNumber;
+function adjustOrderSummary() {
+    for(var i=1;i<items.rows.length;i++){
+        order.deleteRow(i);
     }
-    if(newNumber==0){
-        document.getElementById(name).innerHTML.style.display= "none";
+    for(var j=1;j<items.rows.length;j++){
+        var row = order.insertRow(j);
+        row.insertCell(0).innerHTML = items.rows[j].getElementsByTagName("p").innerHTML; // insert the name ex: Baguette
+        row.insertCell(1).innerHTML = ("x "+items.rows[j].cells[2].innerHTML); // insert units ex: x 3
+        var price = items.rows[j].cells[1].innerHTML.substring(0,4);
+        var units = items.rows[j].cells[2].innerHTML;
+        row.insertCell(2).innerHTML = `${price * units} $`; // insert price ex: 5.99 $
+
     }
 }
 
