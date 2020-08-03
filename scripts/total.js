2e6d75a865c5582ce797e5686b1e72667eed4af6
marcoqlathrop <marcoqlathrop@gmail.com>
2020-08-02 23:18:08 -0400

    Rafi s2 (#49)
    * Update total.js
    * Update index.html

--- scripts/total.js
+++ scripts/total.js
@@ -1,24 +1,20 @@
-
-
 function updateTotalPrice() {
-  var table = document.getElementById('mytable');
+  var table = document.getElementById('itemtable');
 
   var pricePerProduct = []
+  var quantityPerProduct = []
 
   //Get values of the last column, price per product
-  for (var i = 0; i < table.rows.length; i++) {
-    //Ignores the row where there are no items
-    if (table.rows[i].cells[0].innerHTML) {
-      var lastCellValue = table.rows[i].cells[2].innerHTML;
+  for (var i = 1, row; row = table.rows[i]; i++) {
+    //Price of Each product
+    var price = table.rows[i].cells[1].innerHTML
+    var priceInFloat = parseFloat(price.match(/[+-]?\d+(?:\.\d+)?/g));
+    pricePerProduct.push(priceInFloat);
 
-      // Get numbers out of the string
-      var numberOfItemPerThisProduct = parseFloat(lastCellValue.match(/[+-]?\d+(?:\.\d+)?/g));
-      if (numberOfItemPerThisProduct) {
-        pricePerProduct.push(numberOfItemPerThisProduct)
-      } else {
-        pricePerProduct.push(1)
-      }
-    }
+    // Quantity of Each product
+    var quantity = table.rows[i].cells[2].innerHTML
+    var quantityInFloat = parseFloat(quantity)
+    quantityPerProduct.push(quantityInFloat);
   }
 
   var subTotal = 0.0
@@ -29,13 +25,15 @@ function updateTotalPrice() {
 
   // Sub Total is calculated
   for(var r = 0; r < pricePerProduct.length; r++) {
-    subTotal += pricePerProduct[r]
+    subTotal += pricePerProduct[r] * quantityPerProduct[r];
   }
 
   // Delivery Fee is updated depending on the subtotal
   if (subTotal < 30.0) {
     deliveryFee = 12.00
   }
+
+  subTotal = Math.round((subTotal * 1.00) *100)/100;
 
   // Calculate GST Tax
   GST = Math.round((subTotal * 0.05) *100)/100;
@@ -51,12 +49,5 @@ function updateTotalPrice() {
   document.getElementsByClassName("GST")[0].innerHTML = GST + " $"
   document.getElementsByClassName("QST")[0].innerHTML = QST + " $"
   document.getElementsByClassName("grandTotal")[0].innerHTML = total + " $"
-
-
-  // console.log('Sub Total: ' + subTotal);
-  // console.log('Delivery Fee: ' + deliveryFee);
-  // console.log('GST: ' + GST);
-  // console.log('QST: ' + QST);
-  // console.log('Total: ' + total);
 
 }
