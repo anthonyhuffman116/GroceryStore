eb8ce278c73a2019ab090231b75368b97263abd4
marcoqlathrop <marcoqlathrop@gmail.com>
2020-08-16 14:22:33 -0400

    Update p11.php

--- backstore/p11.php
+++ backstore/p11.php
@@ -1,7 +1,3 @@
-<!--
-    P11 -  order list  profile
-    Noor Hammodi - 40061760
--->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,13 +14,11 @@
             Back Store - Order List
         </header>
 
-
-        
         <nav>
             <ul>
-                <li><a href="p7.html">Product list</a></li>
+                <li><a href="p7.php">Product list</a></li>
                 <li><a href="p9.php">User list</a></li>
-                <li><a href="p11.html">Order list</a></li>
+                <li><a href="p11.php">Order list</a></li>
                 <li><a href="../index.html">Main site</a></li>
             </ul>
             <a href="../index.html"><button class="logout-button" type="button" title="logout">Log out</button></a>
@@ -34,109 +28,52 @@
 
         <div id="main-block">
             <h2>Order List</h2>
-            Select an order:
+            Select a order:
             <br><br>
             <form action="p12.php" method="GET">
                 <div class="table-overflow">
                     <table>
                         <tr>
                             <th>Order Number</th>
-                            <th>Customer's id</th>
+                            <th>Customer ID</th>
                             <th>Date created</th>
-                            <th>Date modified</th>
+                            <th>Date modified </th>
                             <th>First Name</th>
                             <th>Last Name</th>
-                            <th>Email</th>
+                            <th>Email </th>
                             <th>Phone number</th>
-                            <th>Order note </th>
                         </tr>
                         <?php
-                        $orderlist=simplexml_load_file("orderlist.xml") or die("Error: cannot load orderlist.xml");
-                        foreach($orderlist->children() as $order){
-                            echo "<tr>";
-                            echo "<td><label for=\"$order->id\"><input type=\"radio\" name=\"selectedOrder\" id=\"$order->id\" value=\"$order->id\" onclick=\"enableBtns()\" />$order->id</label></td>";
-                            echo "<td>$order->ordernum</td>";
-                            echo "<td>$order->customerid</td>";
-                            echo "<td>$order->datec</td>";
-                            echo "<td>$order->datem</td>";
-                            echo "<td>$order->fname</td>";
-                            echo "<td>$order->lname</td>";
-                            echo "<td>$order->email</td>";
-                            echo "<td>$order->phonenum</td>";
-                            echo "<td>$order->orderdesc</td>";
-                            echo "</tr>";
-                        ?></table>
-                       <!-- 
-                        <tr>
-                            <td>
-                                <label for="row1"><input type="radio" name="selectedOrder" id="row1" /> 000123</label>
-                            </td>
-                            <td>John Smith</td>
-                            <td><ul>
-                                <li>2 Lettuces</li>
-                                <li>1 FireCracker Popsicle</li>
-                            </ul></td>
-                            <td> deliver to shipping address </td>
-                            <td><em> **Please drop off at the door</em></td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label for="row2"><input type="radio" name="selectedUser" id="row2" /> 000456</label>
-                            </td>
-                            <td>Jane Doe</td>
-                            <td><ul>
-                                <li>4 Ground Beef</li>
-                                <li>2 Bananas</li>
-                                <li>8 Ben&Jerry's Ice Cream</li>
-                            </ul></td>
-                            <td>pickup at the store</td>
-                            <td><em>**Please put the ice cream in different bag</em></td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label for="row3"><input type="radio" name="selectedUser" id="row3" /> 000789</label>
-                            </td>
-                            <td>Bob Loblaw</td>
-                            <td><ul>
-                                <li>2 Baguettes</li>
-                                <li>5 Chips</li>
-                                <li>1 Ben&Jerry's Ice Cream</li>
-                                <li>5 Cheesedips
-                                </li>
-                            </ul></td>
-                            <td>deliver to shipping address</td>
-                            <td><em>**Please ring dorbell</em></td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label for="row4"><input type="radio" name="selectedUser" id="row4" /> 0000123</label>
-                            </td>
-                            <td>John Doe</td>
-                            <td><ul>
-                                <li>10 Tortillas</li>
-                                <li>10 Ground Beef</li>
-                                <li>15 Chips</li>
-                                <li>7 Cheesedips
-                                </li>
-                            <li>
-                                3 Lettuces
-                            </li>
-                        <li>1 FireCracker Popsicle</li></td>
-                            <td>deliver to shipping address</td>
-                            <td><em>**Please drop off at door</em></td>
-                        </tr>
-                   </table> -->
+                            $orderlist=simplexml_load_file("orderlist.xml") or die("Error: cannot load orderlist.xml");
+
+                            foreach($orderlist->children() as $order){
+                                echo "<tr>";
+                                echo "<td><label for=\"$order->id\"><input type=\"radio\" name=\"selectedUser\" id=\"$order->id\" value=\"$order->id\" onclick=\"enableBtns()\" />$order->id</label></td>";
+                                //echo "<td>$order->id</td>";
+                                echo "<td>$order->customerid</td>";
+                                //echo "<td>$order->ordernum</td>";
+                                
+                                echo "<td>$order->datec</td>";
+                                echo "<td>$order->datem</td>";
+                                echo "<td>$order->fname</td>";
+                                echo "<td>$order->lname</td>";
+                                echo "<td>$order->email</td>";
+                                echo "<td>$order->phonenum</td>";
+                                
+                                echo "</tr>";
+
+                            }
+                        ?>
+                        
+                    </table>
                 </div>
                 <br>
                 <input type="submit" value="Add" name="add" class="button">
-                <input type="submit" value="Delete" name="delete" class="button" id="deleteButton" formaction="deleteorder.php" disabled>
-                <input type="submit" value="Edit" name="edit" id="editButton" class="button" disabled>
-                </form>
-                <br>
+                <input type="submit" value="Delete" name="delete" class="button" id="deleteButton" formaction="deleteuser.php" disabled>
+                <input type="submit" value="Edit" name="edit" id="editButton" class="button" disabled> 
                 
-               
-            
-            
+            </form>
+            <br>
 
         </div>
         <footer>
